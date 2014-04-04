773dce018dd4d0cc99729a3db9e540dfefd76f04
dyep49 <dyep49@gmail.com>
2014-04-04 12:22:39 -0400

    added owl carousel

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$('#responsive-menu-button').sidr({
 		source: '#navigation'
 	});
+
+	$('#project-slider').owlCarousel({
+		singleItem: true,
+		navigation: true
+	});
 });
