e2b95cd96996d42c527e8bc29774ca7c8b376c0e
dyep49 <dyep49@gmail.com>
2014-04-04 17:44:42 -0400

    added touchwipe

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 	$('#responsive-menu-button').sidr({
+		name: 'sidr-main',
 		source: '#navigation'
 	});
+
+  $(window).touchwipe({
+    wipeLeft: function() {
+      // Close
+      $.sidr('close', 'sidr-main');
+    },
+    wipeRight: function() {
+      // Open
+      $.sidr('open', 'sidr-main');
+    },
+    preventDefaultEvents: false
+  });
 
 	$('#project-slider').owlCarousel({
 		singleItem: true,
