d97fd43eeea44f77ba3741feed3d99939a519f4e
dyep49 <dyep49@gmail.com>
2016-03-01 23:08:30 -0500

    add checkboxes for hr

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const $ = require('jquery');
-const jadeData = require('./../../data/projects.json');
+const skills = require('./../../data/skills.json');
+const projects = require('./../../data/projects.json');
+const jadeData = Object.assign({}, skills, projects);
 const template = require('jade!./../index.jade')(jadeData);
 $('body').html(template);
 
 import './../styles/main.scss';
 import './header.js';
-// import languageChart from './language-chart.js';
+import languageChart from './language-chart.js';
+
+// TODO: Fix this. Please.
+setTimeout(() => {
+  languageChart(skills.skills);
+}, 1000);
 
 if (module.hot) {
   module.hot.accept();
--- src/scripts/language-chart.js
+++ src/scripts/language-chart.js
@@ -1,28 +1,75 @@
 const d3 = require('d3');
 
-module.exports = function () {
-  const margin = { top: 30, right: 20, bottom: 30, left: 20 };
-  const containerWidth = parseInt(d3.select('.language-chart').style('width'), 10);
-  const width = containerWidth - margin.left - margin.right;
-  const barHeight = 40;
-  const spacing = 3;
+let margin;
+let containerWidth;
+let width;
+let barHeight;
+let spacing;
+let height;
+let chart;
+let x;
+let y;
+let colorScale;
+let xAxis;
 
-  let height;
+function resize() {
+    // update width
+  const updatedWidthContainer = parseInt(d3.select('.language-chart').style('width'), 10);
+  const updatedWidth = updatedWidthContainer - margin.left - margin.right;
 
+    // resize the chart
+  x.range([0, updatedWidth]);
+  d3.select(chart.node().parentNode)
+        .style('height', (y.rangeExtent()[1] + margin.top + margin.bottom) + 'px')
+        .style('width', (updatedWidth + margin.left + margin.right) + 'px');
+
+  chart.selectAll('.legend')
+      .style('display', () => {
+        const mobile = d3.select('.mobile-legend').style('display') !== 'none';
+        return mobile ? 'none' : 'block';
+      })
+      .attr('transform', (d, i) => {
+        const translateY = (30 * i) + height - 100;
+        const translateX = updatedWidth - width;
+        return 'translate(' + translateX + ',' + translateY + ')';
+      });
+
+  chart.selectAll('rect.background')
+        .attr('width', updatedWidth);
+
+  chart.selectAll('rect.percent')
+        .attr('width', d => x(d.percent / 10));
+
+    // update axes
+  chart.select('.x.axis.top').call(xAxis.orient('top'));
+  chart.select('.x.axis.bottom').call(xAxis.orient('bottom'));
+}
+
+module.exports = function appendChart(data) {
+  margin = { top: 30, right: 20, bottom: 30, left: 20 };
+  containerWidth = parseInt(d3.select('.language-chart').style('width'), 10);
+  width = containerWidth - margin.left - margin.right;
+  barHeight = 40;
+  spacing = 3;
+  // create the chart
+  chart = d3.select('.language-chart').append('svg')
+    .style('width', (width + margin.left + margin.right) + 'px')
+  .append('g')
+    .attr('transform', 'translate(' + [margin.left, margin.top] + ')');
   // scales and axes
-  const x = d3.scale.linear()
+  x = d3.scale.linear()
       .range([0, width])
       .domain([0, 10]); // hard-coding this because I know the data
 
   // ordinal scales are easier for uniform bar heights
   // I'll set domain and rangeBands after data loads
-  const y = d3.scale.ordinal();
+  y = d3.scale.ordinal();
 
-  const colorScale = d3.scale.ordinal()
-                    .domain(['Language', 'Library/Framework', 'Software/Management/Deployment'])
+  colorScale = d3.scale.ordinal()
+                    .domain(['Language', 'Library/Framework', 'Everything Else'])
                     .range(['#0f0', 'magenta', 'cyan']);
 
-  const xAxis = d3.svg.axis()
+  xAxis = d3.svg.axis()
       .scale(x)
       .tickFormat(d => {
         let axisLabel = d;
@@ -42,121 +89,78 @@ module.exports = function () {
         return axisLabel;
       });
 
-  // create the chart
-  const chart = d3.select('.language-chart').append('svg')
-      .style('width', (width + margin.left + margin.right) + 'px')
-    .append('g')
-      .attr('transform', 'translate(' + [margin.left, margin.top] + ')');
 
-
-  function resize() {
-      // update width
-    const updatedWidthContainer = parseInt(d3.select('.language-chart').style('width'), 10);
-    const updatedWidth = updatedWidthContainer - margin.left - margin.right;
-
-      // resize the chart
-    x.range([0, updatedWidth]);
-    d3.select(chart.node().parentNode)
-          .style('height', (y.rangeExtent()[1] + margin.top + margin.bottom) + 'px')
-          .style('width', (updatedWidth + margin.left + margin.right) + 'px');
-
-    chart.selectAll('.legend')
-        .style('display', () => {
-          const mobile = d3.select('.mobile-legend').style('display') !== 'none';
-          return mobile ? 'none' : 'block';
-        })
-        .attr('transform', (d, i) => {
-          const translateY = (30 * i) + height - 100;
-          const translateX = updatedWidth - width;
-          return 'translate(' + translateX + ',' + translateY + ')';
-        });
-
-    chart.selectAll('rect.background')
-          .attr('width', updatedWidth);
-
-    chart.selectAll('rect.percent')
-          .attr('width', d => x(d.percent / 10));
-
-      // update axes
-    chart.select('.x.axis.top').call(xAxis.orient('top'));
-    chart.select('.x.axis.bottom').call(xAxis.orient('bottom'));
-  }
-
-  function appendChart(data) {
     // set y domain
-    y.domain(d3.range(data.length))
-        .rangeBands([0, data.length * barHeight]);
+  y.domain(d3.range(data.length))
+      .rangeBands([0, data.length * barHeight]);
 
-    // set height based on data
-    // height = y.rangeExtent()[1];
-    height = data.length * barHeight;
-    d3.select(chart.node().parentNode)
-        .style('height', (height + margin.top + margin.bottom) + 'px');
+  // set height based on data
+  // height = y.rangeExtent()[1];
+  height = data.length * barHeight;
+  d3.select(chart.node().parentNode)
+      .style('height', (height + margin.top + margin.bottom) + 'px');
 
 
-    // add top and bottom axes
-    chart.append('g')
-        .attr('class', 'x axis top')
-        .call(xAxis.orient('top'));
+  // add top and bottom axes
+  chart.append('g')
+      .attr('class', 'x axis top')
+      .call(xAxis.orient('top'));
 
-    chart.append('g')
-        .attr('class', 'x axis bottom')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis.orient('bottom'));
+  chart.append('g')
+      .attr('class', 'x axis bottom')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis.orient('bottom'));
 
-    const bars = chart.selectAll('.bar')
-        .data(data)
+  const bars = chart.selectAll('.bar')
+      .data(data)
+    .enter().append('g')
+      .attr('class', 'bar')
+      .attr('transform', (d, i) => 'translate(0,' + y(i) + ')');
+
+  bars.append('rect')
+      .attr('class', 'background')
+      .attr('height', y.rangeBand())
+      .attr('width', width);
+
+  bars.append('rect')
+      .attr('class', 'percent')
+      .attr('height', y.rangeBand())
+      .attr('width', d => x(d.percent / 10))
+      .attr('fill', d => colorScale(d.category));
+
+  bars.append('text')
+      .text(d => d.name)
+      .attr('class', 'name')
+      .attr('y', y.rangeBand() - 10)
+      .attr('x', spacing)
+      .style('color', 'white');
+
+  const legend = chart.selectAll('.legend')
+      .data(colorScale.domain())
       .enter().append('g')
-        .attr('class', 'bar')
-        .attr('transform', (d, i) => 'translate(0,' + y(i) + ')');
+      .attr('class', 'legend')
+      .attr('transform', (d, i) => {
+        const translateY = (30 * i) + height - 100;
+        return 'translate(0,' + translateY + ')';
+      })
+      .style('display', () => {
+        const mobile = d3.select('.mobile-legend').style('display') !== 'none';
+        return mobile ? 'none' : 'block';
+      });
 
-    bars.append('rect')
-        .attr('class', 'background')
-        .attr('height', y.rangeBand())
-        .attr('width', width);
+  legend.append('rect')
+      .attr('x', width - 18)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', d => colorScale(d));
 
-    bars.append('rect')
-        .attr('class', 'percent')
-        .attr('height', y.rangeBand())
-        .attr('width', d => x(d.percent / 10))
-        .attr('fill', d => colorScale(d.category));
+  legend.append('text')
+      .attr('x', width - 25)
+      .attr('y', 8)
+      .attr('dy', '.35em')
+      .attr('class', 'legend-text')
+      .style('text-anchor', 'end')
+      .text(d => d);
 
-    bars.append('text')
-        .text(d => d.name)
-        .attr('class', 'name')
-        .attr('y', y.rangeBand() - 10)
-        .attr('x', spacing)
-        .style('color', 'white');
-
-    const legend = chart.selectAll('.legend')
-        .data(colorScale.domain())
-        .enter().append('g')
-        .attr('class', 'legend')
-        .attr('transform', (d, i) => {
-          const translateY = (30 * i) + height - 100;
-          return 'translate(0,' + translateY + ')';
-        })
-        .style('display', () => {
-          const mobile = d3.select('.mobile-legend').style('display') !== 'none';
-          return mobile ? 'none' : 'block';
-        });
-
-    legend.append('rect')
-        .attr('x', width - 18)
-        .attr('width', 18)
-        .attr('height', 18)
-        .style('fill', d => colorScale(d));
-
-    legend.append('text')
-        .attr('x', width - 25)
-        .attr('y', 8)
-        .attr('dy', '.35em')
-        .attr('class', 'legend-text')
-        .style('text-anchor', 'end')
-        .text(d => d);
-
-    d3.select(window).on('resize', resize);
-  }
-
-  d3.json('../../data/skills.json', (err, data) => appendChart(data.skills));
+  d3.select(window).on('resize', resize);
 };
